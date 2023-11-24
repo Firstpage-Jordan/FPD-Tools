791e7232e82e2dd36899e66eb8c55331fa6cff86
Firspage-Jordan <jordan@firstpagedigital.sg>
2023-11-24 15:31:55 +0800

    added logic for sentences that don't start with <p>

--- blog-content-helper/index.js
+++ blog-content-helper/index.js
@@ -1,47 +1,48 @@
 const submitButton = document.querySelector(".submit-button");
 const inputField = document.querySelector(".input--text-area");
 const outputField = document.querySelector(".output--text-area");
-const outputHTML = document.querySelector(".output-html");
+const outputhtml = document.querySelector(".output-html");
 const clearButton = document.querySelector(".clear-button");
-const scrollUpButton = document.querySelector(".scroll-up-button");
-const copyPreviewButton = document.querySelector(".copy-preview-button");
 
 const message = document.querySelector(".message");
 const copyButton = document.querySelector(".copy-button");
 
 submitButton.addEventListener("click", function () {
   let uncleanContent = inputField.value;
-  // clean content
+
+  // split content into lines
   splitContent = uncleanContent.split(/\r?\n/);
-  // console.log(splitContent);
 
   const headers = ["H1", "H2", "H3", "H4", "H5"];
 
   let cleanedContent = splitContent.map((line) => {
     headers.some((header) => {
       if (line.includes(header)) {
-        // console.log(`${line} contains ${header}`);
-        line = line
-          .replace(/<strong>/g, "")
-          .replace(/<\/strong>/g, "")
-          .replace(`${header} `, "")
-          .replace("<p>", `<${header.toLowerCase()}><strong>`)
-          .replace("</p>", `</strong></${header.toLowerCase()}>`);
-
-        // console.log(line);
+        // if the line is a header but has <p> tag
+        if (line.includes("<p>")) {
+          line = line
+            .replace(/<strong>/g, "") // remove all <strong>
+            .replace(/<\/strong>/g, "") // remove all </strong>
+            .replace(`${header} `, "") // remove H2, H3, H4, etc.
+            .replace("<p>", `<${header.toLowerCase()}><strong>`)
+            .replace("</p>", `</strong></${header.toLowerCase()}>`);
+        } else {
+          // if line is a header and already has header tag
+          line = line
+            .replace(/(<[a-z0-9]{1,}>)/g, "") // remove all <>
+            .replace(/(<\/[a-z0-9]{1,}>)/g, "") // remove all </>
+            .replace(`${header} `, "");
+          line = `<${header.toLowerCase()}><strong>` + line;
+          line += `</strong></${header.toLowerCase()}>`;
+        }
       }
     });
 
     // output success message
     message.classList.remove("hide");
 
-    // show copy preview button
-    copyPreviewButton.style.display = "block";
-
     return line;
   });
-
-  // console.log(cleanedContent);
 
   cleanContentJoined = cleanedContent.join("\r\n");
 
@@ -49,23 +50,17 @@ submitButton.addEventListener("click", function () {
   outputField.value = cleanContentJoined;
 
   // output the HTML
-  outputHTML.innerHTML = cleanContentJoined;
+  outputhtml.innerHTML = cleanContentJoined;
 });
 
 copyButton.addEventListener("click", () => {
   let cleanedContent = outputField.value;
-  let originalButtonContent = copyButton.innerHTML;
 
   // copy output content to clipboard
   navigator.clipboard.writeText(cleanedContent);
 
   // edit button text
   copyButton.textContent = "Copied to clipboard!";
-
-  // reset button text to original after 2 seconds
-  setTimeout(() => {
-    copyButton.innerHTML = originalButtonContent;
-  }, 1500);
 });
 
 // focus on input field when window loads
@@ -78,58 +73,5 @@ clearButton.addEventListener("click", () => {
   inputField.value = "";
   outputField.value = "";
   message.classList.add("hide");
-  outputHTML.innerHTML = "";
-  // hide copy preview button
-  copyPreviewButton.style.display = "none";
+  outputhtml.innerHTML = "";
 });
-
-// scroll to top
-scrollUpButton.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
-});
-
-// show scroll button when user scrolls down
-window.addEventListener("scroll", (e) => {
-  scrollUpButton.style.display = window.scrollY > 20 ? "block" : "none";
-});
-
-// copy preview button
-// copyPreviewButton.addEventListener("click", () => {
-//   let previewContent = outputHTML.innerHTML;
-//   let originalButtonContent = copyPreviewButton.innerHTML;
-
-//   // copy output content to clipboard
-//   navigator.clipboard.writeText(previewContent);
-
-//   // edit button text
-//   copyPreviewButton.textContent = "Copied to clipboard!";
-
-//   // reset button text to original after 2 seconds
-//   setTimeout(() => {
-//     copyPreviewButton.innerHTML = originalButtonContent;
-//   }, 1500);
-// });
-
-// copy preview button
-copyPreviewButton.addEventListener("click", copyToClipboard);
-
-function copyToClipboard(id) {
-  let originalButtonContent = copyPreviewButton.innerHTML;
-  let r = document.createRange();
-  r.selectNode(outputHTML);
-  window.getSelection().removeAllRanges();
-  window.getSelection().addRange(r);
-  document.execCommand("copy");
-
-  // edit button text
-  copyPreviewButton.textContent = "Copied to clipboard!";
-
-  // reset button text to original after 2 seconds
-  setTimeout(() => {
-    copyPreviewButton.innerHTML = originalButtonContent;
-  }, 1500);
-}
